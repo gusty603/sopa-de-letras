defe9224a496b9bed1d127b30e855da11e722b16
Gustavo Ramirez <gr34291@gmail.com>
2026-08-06 18:44:35 +0000

    hice un input y el listado de palabras a buscar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,41 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+const [nombre, setNombre]=useState('')
 
+const [arrayNombre, setArrayNombres]=useState([])
+
+function cambiarNombres(e){
+  e.preventDefault();
+  setArrayNombres(arrayNombre.concat(nombre))
+  console.log(arrayNombre)
+  setNombre('')
+}
 
   return (
     <>
-     
+    <div id='contenedor'>
+      <div>
+     <h1>Sopa de Letras</h1>
+     <p>Ingresar hasta 10 palabras enter 3 y 12 caracteres</p>
+ 
+
+ <form onSubmit={cambiarNombres}>
+<input type="text"value={nombre} onChange={(e)=> setNombre(e.target.value)} />
+<input type="submit" value="Enviar" />
+ </form>
+</div>
+<div id='divPalabra'>
+<h2>Palabras</h2>
+{
+arrayNombre.map((a,i)=> <li key={i}>{a}</li>)
+  
+}
+</div>
+ </div>
+ 
     </>
+    
   )
 }
 
